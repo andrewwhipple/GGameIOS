25b1c1072975d46163fa19c44ea38cfa709147c9
Andrew Whipple <android128@gmail.com>
2015-08-18 16:27:46 -0700

    A few minor changes; removing unnecessary code, fixing boolean logic

--- GeoGame/ViewController.swift
+++ GeoGame/ViewController.swift
@@ -20,10 +20,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var audioPlayer = AVAudioPlayer()
     var manager = CLLocationManager()
  
-
-    //Registers the geofences
-    // (Hopefully will be deprecated by initialization of the GGame!
-    
     
     func registerLocations() {
         print("In registerLocations")
@@ -32,42 +28,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             manager.startMonitoringForRegion(region)
         }
         
-        /*var geoCenter = CLLocationCoordinate2D(latitude: 37.424953, longitude: -122.136712)
-        var geoRadius = CLLocationDistance(50.0)
-        var geoID = "meow"
-        geoRegion1 = CLCircularRegion(center: geoCenter, radius: geoRadius, identifier: geoID)
-        
-        //manager.startUpdatingLocation()
-        
-        manager.startMonitoringForRegion(geoRegion1)
-        
-        geoCenter = CLLocationCoordinate2D(latitude: 37.424426, longitude: -122.136829)
-        geoRadius = CLLocationDistance(50.0)
-        geoID = "rawr"
-        geoRegion2 = CLCircularRegion(center: geoCenter, radius: geoRadius, identifier: geoID)
-        
-        //manager.startUpdatingLocation()
-        
-        manager.startMonitoringForRegion(geoRegion2)*/
-        
         print("end of registerlocations")
-        
-        /*
-        
+  
 
-        */
     }
     
     func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
         print("Entered region")
-        let alertController = UIAlertController(title: "iOScreator", message: "Hello world!", preferredStyle: UIAlertControllerStyle.Alert)
+        /*let alertController = UIAlertController(title: "iOScreator", message: "Hello world!", preferredStyle: UIAlertControllerStyle.Alert)
         alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
         
-        self.presentViewController(alertController, animated: true, completion: nil)
+        self.presentViewController(alertController, animated: true, completion: nil)*/
         
-        let regionID = region.identifier
-        
-        let sound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(regionID, ofType: "mp3")!)
+        let sound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(region.identifier, ofType: "mp3")!)
 
         print(sound)
         
@@ -82,31 +55,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     //Currently unused dev tool to visually check location
+    
+    /*
+    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let lati = locations[0].coordinate.latitude
         let longi = locations[0].coordinate.longitude
         
         latLongLabel.text = "Lat: \(lati), Long: \(longi)"
-        
-
-    }
     
-    /*
-    func getCurrentLocation() -> GeoGameLocation {
-        
     }
+    */
     
-    func getCurrentGameState() -> GeoGameState {
-        
-    }
-    
-    func loadLocations() {
-        
-    }
-    
-    func checkSound(location: GeoGameLocation, gameState: GeoGameState) {
-        
-    }*/
     @IBOutlet weak var gameButtonLabel: UIButton!
     
     @IBAction func startGameButton(sender: UIButton) {
@@ -115,7 +75,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         if !gGame.state.gameStarted {
             gGame.state.gameStarted = true
-            gGame.state.gameEnded = true
+            gGame.state.gameEnded = false
             
             manager.delegate = self
             manager.requestAlwaysAuthorization()
@@ -135,11 +95,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     
-    func gameLoop() {
-        //let curLocation = getCurrentLocation()
-        //let curGameState = getCurrentGameState()
-        //checkSound(curLocation, gameState: curGameState)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
