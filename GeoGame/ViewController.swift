0a9177d0d9b0411dc944e13c67e3f84e11a30ff9
Andrew Whipple <android128@gmail.com>
2015-08-18 16:24:11 -0700

    Refactored to make it all actually MVC (putting the Game stuff within a global GGame object and GeoGameState)

--- GeoGame/ViewController.swift
+++ GeoGame/ViewController.swift
@@ -12,25 +12,27 @@ import AVFoundation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    var gGame = GGame()
+    
     @IBOutlet weak var latLongLabel: UILabel!
     
     
-    //Hopfully deprecated by GGameState!
-    var gameStarted = false
-    var gameNotOver = true
-    
     var audioPlayer = AVAudioPlayer()
     var manager = CLLocationManager()
-    
-    var locationsToAudio = ["meow":"sound", "rawr": "sound2"]
-    
-    var geoRegion1 = CLCircularRegion()
-    var geoRegion2 = CLCircularRegion()
+ 
+
     //Registers the geofences
     // (Hopefully will be deprecated by initialization of the GGame!
+    
+    
     func registerLocations() {
         print("In registerLocations")
-        var geoCenter = CLLocationCoordinate2D(latitude: 37.424135, longitude: -122.146621)
+        
+        for region in gGame.regions {
+            manager.startMonitoringForRegion(region)
+        }
+        
+        /*var geoCenter = CLLocationCoordinate2D(latitude: 37.424953, longitude: -122.136712)
         var geoRadius = CLLocationDistance(50.0)
         var geoID = "meow"
         geoRegion1 = CLCircularRegion(center: geoCenter, radius: geoRadius, identifier: geoID)
@@ -39,16 +41,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         manager.startMonitoringForRegion(geoRegion1)
         
-        geoCenter = CLLocationCoordinate2D(latitude: 37.422014, longitude: -122.155054)
+        geoCenter = CLLocationCoordinate2D(latitude: 37.424426, longitude: -122.136829)
         geoRadius = CLLocationDistance(50.0)
         geoID = "rawr"
         geoRegion2 = CLCircularRegion(center: geoCenter, radius: geoRadius, identifier: geoID)
         
         //manager.startUpdatingLocation()
         
-        manager.startMonitoringForRegion(geoRegion2)
+        manager.startMonitoringForRegion(geoRegion2)*/
         
         print("end of registerlocations")
+        
+        /*
+        
+
+        */
     }
     
     func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
@@ -60,13 +67,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         let regionID = region.identifier
         
-        let alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(locationsToAudio[regionID], ofType: "mp3")!)
+        let sound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(regionID, ofType: "mp3")!)
 
-        print(alertSound)
+        print(sound)
         
 
         do {
-           try audioPlayer = AVAudioPlayer(contentsOfURL: alertSound)
+           try audioPlayer = AVAudioPlayer(contentsOfURL: sound)
         } catch {
             
         }
@@ -74,6 +81,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         audioPlayer.play()
     }
     
+    //Currently unused dev tool to visually check location
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let lati = locations[0].coordinate.latitude
         let longi = locations[0].coordinate.longitude
@@ -102,20 +110,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var gameButtonLabel: UIButton!
     
     @IBAction func startGameButton(sender: UIButton) {
-        if !gameStarted {
-            gameStarted = true
-            gameNotOver = true
+        
+        
+        
+        if !gGame.state.gameStarted {
+            gGame.state.gameStarted = true
+            gGame.state.gameEnded = true
             
             manager.delegate = self
             manager.requestAlwaysAuthorization()
             //loadLocations()
             registerLocations()
             gameButtonLabel.setTitle("Stop Game", forState: UIControlState.Normal)
-            gameLoop()
+            
         } else {
-            gameStarted = false
-            manager.stopMonitoringForRegion(geoRegion1)
-            manager.stopMonitoringForRegion(geoRegion2)
+            gGame.state.gameStarted = false
+            gGame.state.gameEnded = true
+            for region in gGame.regions {
+                manager.stopMonitoringForRegion(region)
+            }
+            gameButtonLabel.setTitle("Start Game", forState: UIControlState.Normal)
         }
     
     }
@@ -129,6 +143,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
         
     }
 
